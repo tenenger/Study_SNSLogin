9012c45384739c08bfdff8aa38b1fbf92f648549
tenenger <tenenger7125@gmail.com>
2022-06-10 15:13:16 +0900

    feat: kakao sns login end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [isLogin, setIsLogin] = useState(false);
   const { Kakao } = window;
-  const initKakao = () => {
+  const initKakao = async () => {
     const jsKey = "3c5fd0d61672a00438664be501823461";
     if (Kakao && !Kakao.isInitialized()) {
-      Kakao.init(jsKey);
-      console.log(Kakao.isInitialized());
+      await Kakao.init(jsKey);
+      console.log(`kakao 초기화 ${Kakao.isInitialized()}`);
     }
   };
-  const kakaoLogin = () => {
-    Kakao.Auth.login({
-      success() {
+  const kakaoLogin = async () => {
+    await Kakao.Auth.login({
+      success(res) {
+        console.log(res);
+        Kakao.Auth.setAccessToken(res.access_token);
+        console.log("카카오 로그인 성공");
+
         Kakao.API.request({
           url: "/v2/user/me",
           success(res) {
-            alert(JSON.stringify(res));
+            console.log("카카오 인가 요청 성공");
             const kakaoAccount = res.kakao_account;
-            console.log(kakaoAccount);
+            localStorage.setItem("email", kakaoAccount.email);
+            localStorage.setItem(
+              "profileImg",
+              kakaoAccount.profile.profile_image_url
+            );
+            localStorage.setItem("nickname", kakaoAccount.profile.nickname);
+            setIsLogin(true);
           },
           fail(error) {
             console.log(error);
@@ -31,20 +43,57 @@ function App() {
     });
   };
 
+  const kakaoLogout = () => {
+    Kakao.Auth.logout((res) => {
+      console.log(Kakao.Auth.getAccessToken());
+      console.log(res);
+      localStorage.removeItem("email");
+      localStorage.removeItem("profileImg");
+      localStorage.removeItem("nickname");
+      setUser(null);
+      setIsLogin(false);
+    });
+  };
+
   useEffect(() => {
     initKakao();
+    Kakao.Auth.getAccessToken() ? setIsLogin(true) : setIsLogin(false);
   }, []);
+
+  useEffect(() => {
+    console.log(isLogin);
+    if (isLogin) {
+      setUser({
+        email: localStorage.getItem("email"),
+        profileImg: localStorage.getItem("profileImg"),
+        nickname: localStorage.getItem("nickname"),
+      });
+    }
+  }, [isLogin]);
 
   return (
     <div className="App">
       <h2>카카오 로그인 구현</h2>
-      <button onClick={kakaoLogin}>
-        <img
-          src="//k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg"
-          width="222"
-          alt="카카오 로그인 버튼"
-        />
-      </button>
+      {user ? (
+        <div>
+          <button onClick={kakaoLogout}>로그아웃</button>
+          <h2>카카오 로그인 성공!</h2>
+          <h3>카카오 프로필 사진</h3>
+          <img src={user.profileImg} alt="" />
+          <h3>카카오 닉네임</h3>
+          <h4>{user.nickname}</h4>
+          <h3>카카오 이메일</h3>
+          <h4>{user.email}</h4>
+        </div>
+      ) : (
+        <button onClick={kakaoLogin}>
+          <img
+            src="//k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg"
+            width="222"
+            alt="카카오 로그인 버튼"
+          />
+        </button>
+      )}
     </div>
   );
 }
